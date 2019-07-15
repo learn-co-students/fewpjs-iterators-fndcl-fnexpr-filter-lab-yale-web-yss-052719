a26d4aee980f6b955b17bd703d0598b408fd79f9
Elijah <efloren@siue.edu>
2019-07-15 17:07:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+ return drivers.filter(driver => {
+     debugger
+    return driver.toLowerCase() === name.toLowerCase()
+ })
+}
+
+
+function fuzzyMatch(drivers, letter){
+  return drivers.filter(driver => {
+     return driver.slice(0,letter.length).toLowerCase() === letter.toLowerCase()
+ })
+}
+
+function matchName(drivers, name){
+ return drivers.filter(driver => {
+      return driver.name === name 
+    })
+
+}
