395e953a5c6d4d774e00faaf8de9c08be1a0b884
XRuiz9 <xdruiz92@gmail.com>
2019-07-21 16:15:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, name) {
+	return array.filter(driver => {
+		return driver.toLowerCase() === name.toLowerCase()
+	})
+}
+
+function fuzzyMatch(array, substr) {
+	return array.filter(driver => {
+		return driver.substring(0, substr.length) === substr
+	})
+}
+
+function matchName(array, string) {
+	return array.filter(driver => {
+		return driver.name === string
+	})
+}
